a92e06292e7ce4133f14602dfc7c81e6e2aa92c5
kkami <kkamidog@naver.com>
2024-04-18 11:05:54 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todoValueTemp, setTodoValueTemp] = useState('');
+  const [todoValues, setTodoValues] = useState([]);
+
+  const inputValue = (e) => {
+    setTodoValueTemp(e.target.value);
+  };
+  const onRegistrationButton = () => {
+    setTodoValues([...todoValues, { id: Date.now(), text: todoValueTemp }]);
+    console.log(todoValues);
+    setTodoValueTemp('');
+  };
+
+  return (
+    <>
+      <div className='wrap'>
+        <div className='todo'>
+          <div className='todo__inputFunctions'>
+            <input
+              type='text'
+              className='todo__input'
+              onChange={inputValue}
+              value={todoValueTemp}
+            />
+            <button
+              type='button'
+              className='todo__btn'
+              onClick={onRegistrationButton}
+            >
+              등록
+            </button>
+          </div>
+          <div className='todo__lists'>
+            <table className='todo__table'>
+              <colgroup>
+                <col width={100} />
+                <col width={'auto'} />
+                <col width={100} />
+                <col width={100} />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>번호</th>
+                  <th>할 일</th>
+                  <th>수정</th>
+                  <th>삭제</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* {todoValues.map((item, i) => {
+                  return (
+                    <tr className='todo__tr' key={i}>
+                      <td>No.{i + 1}</td>
+                      <td>{item}</td>
+                      <td>
+                        <button type='button'>수정</button>
+                      </td>
+                      <td>
+                        <button type='button' onClick={deletegg}>
+                          삭제
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })} */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
